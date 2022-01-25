7135281df3235c006aabd20bf8cf031a792708bd
Rostislav <spellmane21@gmail.com>
2022-01-25 11:57:08 +0200

    #5 about page

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -6,9 +6,12 @@ const About: React.FC = () => {
         <main className={s.main}>
             <PageName name='About' />
             <div className={s.post}>
-                <div className='row'>
+                <div className={'row' + ' ' + s.margin}>
                     <span className={s.slash}>/</span>
                     <h1 className={s.title}>Our History</h1>
+                </div>
+                <div className={s.text}>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum vero laudantium autem sit assumenda voluptate impedit, magnam, exercitationem quis, deserunt beatae non aliquid labore tenetur sed quod doloremque quidem nihil. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum vero laudantium autem sit assumenda voluptate impedit, magnam, exercitationem quis, deserunt beatae non aliquid labore tenetur sed quod doloremque quidem nihil.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum vero laudantium autem sit assumenda voluptate impedit, magnam, exercitationem quis, deserunt beatae non aliquid labore tenetur sed quod doloremque quidem nihil.
                 </div>
             </div>
         </main>
