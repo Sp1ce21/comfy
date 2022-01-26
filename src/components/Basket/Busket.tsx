88673a632b9068f93b1ac7dd9aafa72df472f230
Rostislav <spellmane21@gmail.com>
2022-01-26 16:23:12 +0200

    #18 basket/ added animation of opening

--- src/components/Basket/Busket.tsx
+++ src/components/Basket/Busket.tsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react';
 import s from './Busket.module.css'
 import { Props } from './BusketContainer';
 
 const Busket: React.FC<Props> = ({ setIsBasket }) => {
+
+    let [myTimeout, setMyTimeout] = useState(true)
+    let [isActiveBasket, setIsActiveBasket] = useState(false)
+
+    useEffect(() => {
+        if (myTimeout) {
+            setTimeout(() => {
+                setMyTimeout(false)
+                setIsActiveBasket(true)
+            }, 100);
+        }
+    })
+
     return (
-        <div className={s.busket}>
+        <div className={isActiveBasket ? s.active : s.busket}>
             hello!
         </div>
     )
