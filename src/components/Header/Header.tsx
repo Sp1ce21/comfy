7dc80acfc7d7e2227220ac19e5d246c6e9e1dd69
Rostislav <spellmane21@gmail.com>
2022-01-28 12:46:43 +0200

    #26 header/ added basket animation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,12 +5,15 @@ import { NavLink } from 'react-router-dom';
 import cart from '../../assets/images/cart.svg'
 import { Props } from './HeaderContainer';
 
-const Header: React.FC<Props> = ({setIsBasket}) => {
+const Header: React.FC<Props> = ({ setIsBasket, addedProducts }) => {
     return (
         <header className={s.header}>
-            <Navigation/>
-            <NavLink to='' className={s.row}><img src={logo} alt="logo" className={s.image}/></NavLink>
-            <div className={s.container}><img src={cart} alt="cart" onClick={()=>setIsBasket(true)}/></div>
+            <Navigation />
+            <NavLink to='' className={s.row}><img src={logo} alt="logo" className={s.image} /></NavLink>
+            <div className={s.container}>
+                { addedProducts.length !==0 && <div className={s.circle}>{addedProducts.length}</div> }
+                <img src={cart} alt="cart" onClick={() => setIsBasket(true)} />
+            </div>
         </header>
     )
 }
--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -4,16 +4,19 @@ import { setIsBasket } from '../../redux/products-reducer'
 import { appStateType } from "../../redux/store";
 
 export type Props = {
+    addedProducts: any
+
     setIsBasket: (isBasket: boolean)=>void
 }
 
-const HeaderContainer: React.FC<Props> = ({ setIsBasket }) => {
+const HeaderContainer: React.FC<Props> = ({ setIsBasket, addedProducts }) => {
     return (
-        <Header setIsBasket={setIsBasket} />
+        <Header setIsBasket={setIsBasket} addedProducts={addedProducts}/>
     )
 }
 
 let mapStateToProps = (state: appStateType) => ({
+    addedProducts: state.productsPage.addedProducts
 });
 
 export default connect(mapStateToProps, { setIsBasket })(HeaderContainer);
