cb817fe21166e8cea6cb50bcdd330c567785f13b
Rostislav <spellmane21@gmail.com>
2022-01-31 14:58:15 +0200

    #32 added button "added" in product items

--- src/common/ProductItem/ProductItem.tsx
+++ src/common/ProductItem/ProductItem.tsx
@@ -1,24 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { productsObject } from '../../redux/products-reducer'
 import s from './ProductItem.module.css'
 
 type Props = {
     product: productsObject
     isBasket: boolean
+    addedProducts: any
 
     setAddedProducts: (product: productsObject) => void
 }
 
-const ProductItem: React.FC<Props> = ({ isBasket, setAddedProducts, product }) => {
+const ProductItem: React.FC<Props> = ({ isBasket, setAddedProducts, product, addedProducts }) => {
+
 
     let [isButton, setIsButton] = useState(false)
+
 
     return (
         <div className={s.item} onMouseEnter={() => { !isBasket && setIsButton(true) }} onMouseLeave={() => { !isBasket && setIsButton(false) }} >
             <div className={s.column}>
                 <div className={s.imgContainer}>
-                    {isButton && <div className={s.button} onClick={() => { setAddedProducts(product) }}>Add to cart</div>}
-                    <img src={product.image} alt="" />
+                    {addedProducts.length !== 0 ? addedProducts.map((addedProduct: productsObject) => {
+                        return (
+                            addedProduct.id === product.id && isButton && <div className={s.button}>Added</div> 
+                            || 
+                            isButton && <div className={s.button} onClick={() => { setAddedProducts(product) }}>Add to cart</div>
+                        )
+                    })
+                        : isButton && <div className={s.button} onClick={() => { setAddedProducts(product) }}>Add to cart</div>
+                    }
+                    <img src={product.image} alt="Product image" />
                 </div>
                 <div className={s.title}>{product.title}</div>
             </div>
--- src/common/ProductItem/ProductItemContainer.tsx
+++ src/common/ProductItem/ProductItemContainer.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import { connect } from "react-redux"
 import { productsObject, setAddedProducts } from "../../redux/products-reducer"
 import { appStateType } from "../../redux/store"
@@ -6,19 +7,23 @@ import ProductItem from "./ProductItem"
 export type Props = {
     product: productsObject
     isBasket: boolean
+    addedProducts: any
 
     setAddedProducts: (product: productsObject) => void
 }
 
-const ProductContainer: React.FC<Props> = ({ product, isBasket, setAddedProducts }) => {
+const ProductContainer: React.FC<Props> = ({ product, isBasket, addedProducts, setAddedProducts }) => {
+
+
     return (
-        <ProductItem product={product} isBasket={isBasket} setAddedProducts={setAddedProducts} />
+        <ProductItem product={product} isBasket={isBasket} setAddedProducts={setAddedProducts} addedProducts={addedProducts}/>
     )
 }
 
 let mapStateToProps = (state: appStateType) => ({
     products: state.productsPage.products,
     isBasket: state.productsPage.isBasket,
+    addedProducts: state.productsPage.addedProducts,
 });
 
 export default connect(mapStateToProps, { setAddedProducts })(ProductContainer);
