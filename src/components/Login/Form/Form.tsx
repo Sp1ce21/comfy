0d601083558923881505bf87b9e80b3f6ee4b447
Rostislav <spellmane21@gmail.com>
2022-02-01 14:35:36 +0200

    #40 login / stylized form

--- src/components/Login/Form/Form.tsx
+++ src/components/Login/Form/Form.tsx
@@ -1,6 +1,7 @@
 import { Field, reduxForm } from 'redux-form';
 import { Input } from '../../../common/FormsControls/FormsControls'
 import { required, maxLength, requiredA, number, isUpperCase, isLowerCase } from '../../../utils/validators/validators';
+import s from './Form.module.css'
 
 type Props = {
 
@@ -8,20 +9,22 @@ type Props = {
 
 const maxLength30 = maxLength(30)
 
-const Form: React.FC<any> = ({ handleSubmit, error }) => {
+const Form: React.FC<any> = ({ handleSubmit }) => {
 
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className={s.form}>
             <Field validate={[
                 required, maxLength30, requiredA
-            ]} name={'email'} placeholder={'Enter email...'} component={Input} type={'email'} />
+            ]} name={'email'} placeholder={'Enter email...'} component={Input} type={'email'} className={s.email} />
             <Field validate={[
                 required, maxLength30, isUpperCase, isLowerCase, number
-            ]} name={'password'} type={'password'} placeholder={'Enter password...'} component={Input} />
-            <Field name={'rememberMe'} type={'checkbox'} component={'input'} /> remember me
-            {error && <div>{error}</div>}
-            <button>Login</button>
+            ]} name={'password'} type={'password'} placeholder={'Enter password...'} component={Input} className={s.password} />
+            <div className={s.checkboxContainer}>
+                <Field name={'rememberMe'} type={'checkbox'} component={'input'} />
+                <label> remember me </label>
+            </div>
+            <button className={s.button}>Login</button>
         </form>
     )
 }
